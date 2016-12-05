f57531e1b11782e10359a6db552be521e802d9fe
AlexDomson <alex@4ats.ru>
2016-12-05 23:39:59 +0200

    05.12

--- js/main.js
+++ js/main.js
@@ -16,31 +16,37 @@ $(function() {
 
     $(window).resize(function (){
         var $width = $('.block').width();
-        $('.before-block').width($width - 3);
-        $('.after-block').width($width - 3);
+        $('.before-block').width($width);
+        $('.after-block').width($width);
     });
     var $width = $('.block').width();
-    $('.before-block').width($width - 3);
-    $('.after-block').width($width - 3);
+    $('.before-block').width($width );
+    $('.after-block').width($width );
 
     $(window).resize(function (){
         var $width2 = $('.before-block').height();
         var $heightmenu = $('.block').height();
-        $('.test1').width($width2 * 1);
-        $('.test2').width($width2 * 1);
-        $('.lefttabs').height($heightmenu * 0.1);
-        $('.news').height($heightmenu * 0.1);
+        $('.test1').width($width2 * 1.8);
+        $('.test2').width($width2 * 1.8);
+        $('.lefttabs').height($heightmenu * 0.065);
+        $('.news').height($heightmenu * 0.075);
         $('.img-cont').height($heightmenu * 0.5);
+        $('.circle').height($heightmenu * 0.2);
+        $('.circle').width($heightmenu * 0.2);
         $('.loader').height($heightmenu);
+        $(".customer_arrow").height($heightmenu * 0.07);
     });
     var $width2 = $('.before-block').height();
     var $heightmenu = $('.block').height();
-    $('.test1').width($width2 * 1);
-    $('.test2').width($width2 * 1);
-    $('.lefttabs').height($heightmenu * 0.1);
-    $('.news').height($heightmenu * 0.1);
+    $('.test1').width($width2 * 1.8);
+    $('.test2').width($width2 * 1.8);
+    $('.lefttabs').height($heightmenu * 0.065);
+    $('.news').height($heightmenu * 0.075);
     $('.img-cont').height($heightmenu * 0.5);
+    $('.circle').height($heightmenu * 0.2);
+    $('.circle').width($heightmenu * 0.2);
     $('.loader').height($heightmenu);
+    $(".customer_arrow").height($heightmenu * 0.07);
 
     var $kube = $("#kube");
     $(".logo-animate, .logo-animate2").click(function(){
@@ -52,6 +58,19 @@ $(function() {
         else {
             $kube.removeClass("kube-white").addClass("kube");
         }
+    });
+
+    $('#carousel_1').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll:1,
+        adaptiveHeight: true,
+        verticalSwiping: true,
+        dots: false,
+        vertical: true,
+        prevArrow: '<a href="#" class="arrow_cont prev"><img src="images/left_arrow.png" alt="" class="customer_arrow" /></a>',
+        nextArrow: '<a href="#" class="arrow_cont next"><img src="images/right_arrow.png" alt=""  class="customer_arrow" /></a>'
     });
 
     var $hid = $(".media-cont");
@@ -124,7 +143,19 @@ $(function() {
             $('.tab-panels>.media-cont>div').hide().filter(this.hash).show();   // - показывается блок с описанием текущей вкладки
             $('.tab-nav li').removeClass('select');                 // - удаляется класс 'select' у активной ранее вкладки
             $(this).parent().addClass('select');                    // - добавляется класс 'select' для выбранной вкладки
+            // $('.scale_small').animate({scrollTop: 0}, 1500);
             return (false);                                         // - прерывается обработка события onClick
-        })
-    })
+        });
+    });
+
+    $('[href^="#"]').on("click", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+        //анимируем переход на расстояние - top за 1500 мс
+        $('.scale_small').animate({scrollTop: 0}, 100);
+    });
 });
