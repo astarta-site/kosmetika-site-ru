857deb66a76c9f2b691384d8ae2083ade1f274ca
AlexDomson <alex@4ats.ru>
2016-12-12 18:00:08 +0200

    12.12

--- js/main.js
+++ js/main.js
@@ -139,7 +139,7 @@ $(function() {
     $(function(){
         $('.tab-nav li:first').addClass('select');                // Первой вкладке добавляетсякласс select
         $('.tab-panels>.media-cont>div').hide().filter(':first').show();      // Скрываются все блоки с описанием вкладок кроме первого
-        $('.tab-nav a').click(function(){                         // При клике на вкладку
+        $('.tab-nav a[href^="#"]').click(function(){                         // При клике на вкладку
             $('.tab-panels>.media-cont>div').hide().filter(this.hash).show();   // - показывается блок с описанием текущей вкладки
             $('.tab-nav li').removeClass('select');                 // - удаляется класс 'select' у активной ранее вкладки
             $(this).parent().addClass('select');                    // - добавляется класс 'select' для выбранной вкладки
